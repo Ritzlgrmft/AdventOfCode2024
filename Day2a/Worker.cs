6baa143cd4871ded605614dd900b3a4cca8fd429
Markus Wagner <markus.wagner02@web.de>
2024-12-02 20:21:39 +0100

    add day2

---
+++ Day2a/Worker.cs
@@ -0,0 +1,42 @@
+﻿using System.Runtime.ExceptionServices;
+
+namespace AdventOfCode2024.Day2a;
+
+public class Worker : IWorker
+{
+    public long DoWork(string inputFile)
+    {
+        var safeReports = 0;
+        foreach (var line in File.ReadLines(inputFile))
+        {
+            var levels = line.Split(" ", StringSplitOptions.RemoveEmptyEntries).Select(l => int.Parse(l)).ToList();
+            if (IsReportSafe(levels, 1, 3) || IsReportSafe(levels, -1, -3))
+            {
+                safeReports++;
+            }
+        }
+
+        return safeReports;
+    }
+
+    bool IsReportSafe(List<int> levels, int min, int max)
+    {
+        for (var i = 0; i < levels.Count - 1; i++)
+        {
+            var diff = levels[i + 1] - levels[i];
+            if (min > 0 && diff >= min && diff <= max)
+            {
+                // ok
+            }
+            else if (min < 0 && diff <= min && diff >= max)
+            {
+                // ok
+            }
+            else
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+}
---
+++ Day2b/Worker.cs
@@ -0,0 +1,71 @@
+﻿namespace AdventOfCode2024.Day2b;
+
+public class Worker : IWorker
+{
+    public long DoWork(string inputFile)
+    {
+        var safeReports = 0;
+        foreach (var line in File.ReadLines(inputFile))
+        {
+            var levels = line.Split(" ", StringSplitOptions.RemoveEmptyEntries).Select(l => int.Parse(l)).ToList();
+            if (IsReportSafe(levels))
+            {
+                safeReports++;
+            }
+        }
+
+        return safeReports;
+    }
+
+    bool IsReportSafe(List<int> levels)
+    {
+        var result = CheckReport(levels);
+        if (!result)
+        {
+            for (var j = 0; j < levels.Count; j++)
+            {
+                var dampenedLevels = levels
+                .Select((level, index) => (level, index))
+                .Where(x => x.index != j)
+                .Select(x => x.level)
+                .ToList();
+                result = result || CheckReport(dampenedLevels);
+            }
+        }
+        return result;
+    }
+
+    private bool CheckReport(List<int> levels)
+    {
+        int min, max;
+        if (levels[0] < levels[1])
+        {
+            min = 1;
+            max = 3;
+        }
+        else if (levels[0] > levels[1])
+        {
+            min = -1;
+            max = -3;
+        }
+        else
+        {
+            return false;
+        }
+
+        for (var i = 0; i < levels.Count - 1; i++)
+        {
+            var diff = levels[i + 1] - levels[i];
+            if ((min > 0 && diff >= min && diff <= max) || (min < 0 && diff <= min && diff >= max))
+            {
+                // check next level
+            }
+            else
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+}
+
