615b0b8edd67396ba05a4a19d4402ef6d8180452
Markus Wagner <markus.wagner02@web.de>
2024-12-05 07:10:26 +0100

    add day4

---
+++ Day4a/Worker.cs
@@ -0,0 +1,58 @@
+﻿using System.Runtime.ExceptionServices;
+using System.Text.RegularExpressions;
+
+namespace AdventOfCode2024.Day4a;
+
+public class Worker : IWorker
+{
+    List<char[]> letters = new List<char[]>();
+    char[] word = "XMAS".ToCharArray();
+
+    public long DoWork(string inputFile)
+    {
+        foreach (var line in File.ReadLines(inputFile))
+        {
+            letters.Add(line.ToCharArray());
+        }
+
+        var count = 0;
+        for (var y = 0; y < letters.Count; y++)
+        {
+            for (var x = 0; x < letters[y].Length; x++)
+            {
+                count += CountWord(0, x, y, 1, 0) + CountWord(0, x, y, -1, 0)
+                + CountWord(0, x, y, -1, 1) + CountWord(0, x, y, 0, 1) + CountWord(0, x, y, 1, 1)
+                + CountWord(0, x, y, -1, -1) + CountWord(0, x, y, 0, -1) + CountWord(0, x, y, 1, -1);
+            }
+        }
+        return count;
+    }
+
+    char GetLetter(int x, int y)
+    {
+        if (y < 0 || y >= letters.Count || x < 0 || x >= letters[y].Length)
+        {
+            return '-';
+        }
+        else
+        {
+            return letters[y][x];
+        }
+    }
+
+    private int CountWord(int index, int x, int y, int deltaX, int deltaY)
+    {
+        if (GetLetter(x, y) != word[index])
+        {
+            return 0;
+        }
+        else if (index == word.Length - 1)
+        {
+            return 1;
+        }
+        else
+        {
+            return CountWord(index + 1, x + deltaX, y + deltaY, deltaX, deltaY);
+        }
+    }
+}
---
+++ Day4b/Worker.cs
@@ -0,0 +1,50 @@
+﻿using System.Runtime.ExceptionServices;
+using System.Text.RegularExpressions;
+
+namespace AdventOfCode2024.Day4b;
+
+public class Worker : IWorker
+{
+    List<char[]> letters = new List<char[]>();
+    char[] word = "XMAS".ToCharArray();
+
+    public long DoWork(string inputFile)
+    {
+        foreach (var line in File.ReadLines(inputFile))
+        {
+            letters.Add(line.ToCharArray());
+        }
+
+        var count = 0;
+        for (var y = 0; y < letters.Count; y++)
+        {
+            for (var x = 0; x < letters[y].Length; x++)
+            {
+                if (GetLetter(x, y) == 'A')
+                {
+                    if (
+                         ((GetLetter(x - 1, y - 1) == 'M' && GetLetter(x + 1, y + 1) == 'S') || (GetLetter(x - 1, y - 1) == 'S' && GetLetter(x + 1, y + 1) == 'M'))
+                         && ((GetLetter(x - 1, y + 1) == 'M' && GetLetter(x + 1, y - 1) == 'S') || (GetLetter(x - 1, y + 1) == 'S' && GetLetter(x + 1, y - 1) == 'M'))
+                    )
+                    {
+                        count++;
+                    }
+                }
+
+            }
+        }
+        return count;
+    }
+
+    char GetLetter(int x, int y)
+    {
+        if (y < 0 || y >= letters.Count || x < 0 || x >= letters[y].Length)
+        {
+            return '-';
+        }
+        else
+        {
+            return letters[y][x];
+        }
+    }
+}
