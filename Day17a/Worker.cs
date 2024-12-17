58b91344be306c0b2c5835a454d76241aee53a49
Markus Wagner <markus.wagner02@web.de>
2024-12-17 17:18:13 +0100

    add day17

---
+++ Day17a/Worker.cs
@@ -0,0 +1,94 @@
+﻿namespace AdventOfCode2024.Day17a;
+
+public class Worker : IWorker
+{
+    uint a, b, c;
+    uint[] program = [];
+
+    public long DoWork(string inputFile)
+    {
+        var lines = File.ReadAllLines(inputFile);
+        a = uint.Parse(lines[0].Split(": ")[1]);
+        b = uint.Parse(lines[1].Split(": ")[1]);
+        c = uint.Parse(lines[2].Split(": ")[1]);
+        program = lines[4].Split([':', ','], StringSplitOptions.TrimEntries).Skip(1).Select(n => uint.Parse(n)).ToArray();
+
+        var instructionPointer = 0;
+        var output = new List<long>();
+        while (instructionPointer < program.Length)
+        {
+            var operand = program[instructionPointer + 1];
+            switch (program[instructionPointer])
+            {
+                case 0:
+                    // adv
+                    a = (uint)Math.Truncate(a / Math.Pow(2, GetComboOperand(operand)));
+                    instructionPointer += 2;
+                    break;
+                case 1:
+                    // bxl
+                    b ^= operand;
+                    instructionPointer += 2;
+                    break;
+                case 2:
+                    // bst
+                    b = GetComboOperand(operand) % 8;
+                    instructionPointer += 2;
+                    break;
+                case 3:
+                    // jnz
+                    if (a == 0)
+                    {
+                        instructionPointer += 2;
+                    }
+                    else
+                    {
+                        instructionPointer = (int)operand;
+                    }
+                    break;
+                case 4:
+                    // bxc
+                    b ^= c;
+                    instructionPointer += 2;
+                    break;
+                case 5:
+                    // out
+                    output.Add(GetComboOperand(operand) % 8);
+                    instructionPointer += 2;
+                    break;
+                case 6:
+                    // bdv
+                    b = (uint)Math.Truncate(a / Math.Pow(2, GetComboOperand(operand)));
+                    instructionPointer += 2;
+                    break;
+                case 7:
+                    // cdv
+                    c = (uint)Math.Truncate(a / Math.Pow(2, GetComboOperand(operand)));
+                    instructionPointer += 2;
+                    break;
+            }
+
+        }
+
+        Console.WriteLine(string.Join(',', output.Select(n => n.ToString())));
+        return 0;
+    }
+
+    uint GetComboOperand(uint operand)
+    {
+        switch (operand)
+        {
+            case 4:
+                return a;
+            case 5:
+                return b;
+            case 6:
+                return c;
+            case 7:
+                throw new NotImplementedException();
+            default:
+                return operand;
+        }
+    }
+
+}
---
+++ Day17b/Worker.cs
@@ -0,0 +1,104 @@
+﻿using System.Text.RegularExpressions;
+
+namespace AdventOfCode2024.Day17b;
+
+public class Worker : IWorker
+{
+    long a, b, c;
+    public long DoWork(string inputFile)
+    {
+        var lines = File.ReadAllLines(inputFile);
+        b = long.Parse(lines[1].Split(": ")[1]);
+        c = long.Parse(lines[2].Split(": ")[1]);
+        var programComplete = lines[4].Split(' ')[1];
+        var program = programComplete.Split(',').Select(n => long.Parse(n)).ToArray();
+
+        long i = 0;
+        while (i < long.MaxValue)
+        {
+            a = i;
+            var instructionPointer = 0;
+            var output = new List<long>();
+            while (instructionPointer < program.Length)
+            {
+                var operand = program[instructionPointer + 1];
+                switch (program[instructionPointer])
+                {
+                    case 0:
+                        // adv
+                        a = (long)Math.Truncate(a / Math.Pow(2, GetComboOperand(operand)));
+                        instructionPointer += 2;
+                        break;
+                    case 1:
+                        // bxl
+                        b ^= operand;
+                        instructionPointer += 2;
+                        break;
+                    case 2:
+                        // bst
+                        b = GetComboOperand(operand) % 8;
+                        instructionPointer += 2;
+                        break;
+                    case 3:
+                        // jnz
+                        if (a == 0)
+                        {
+                            instructionPointer += 2;
+                        }
+                        else
+                        {
+                            instructionPointer = (int)operand;
+                        }
+                        break;
+                    case 4:
+                        // bxc
+                        b ^= c;
+                        instructionPointer += 2;
+                        break;
+                    case 5:
+                        // out
+                        output.Add(GetComboOperand(operand) % 8);
+                        instructionPointer += 2;
+                        break;
+                    case 6:
+                        // bdv
+                        b = (long)Math.Truncate(a / Math.Pow(2, GetComboOperand(operand)));
+                        instructionPointer += 2;
+                        break;
+                    case 7:
+                        // cdv
+                        c = (long)Math.Truncate(a / Math.Pow(2, GetComboOperand(operand)));
+                        instructionPointer += 2;
+                        break;
+                }
+            }
+
+            // each iteration outputs only based on the lowest 3 bits, and divides a by 8 after each iteration so the lowest 3 bits are thrown out. 
+            // thus each iteration is completely independent. so i loop, starting with a = 0, incrementing by one each time until i find an a that outputs the last instruction.
+            // then i multiply it by 8 and iterate starting with that value until i get the last two instructions. i repeat this process until i get the entire program.
+
+            // true if output is identical to the end of program
+            var match = program.TakeLast(output.Count).Zip(output, (p, o) => p == o).All(b => b);
+            if (match && output.Count == program.Length)
+            {
+                break;
+            }
+
+            i = match && i > 0 ? i * 8 : i + 1;
+        }
+        return i;
+    }
+
+    long GetComboOperand(long operand)
+    {
+        return operand switch
+        {
+            4 => a,
+            5 => b,
+            6 => c,
+            7 => throw new NotImplementedException(),
+            _ => operand,
+        };
+    }
+
+}
