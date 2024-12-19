325b48738b4f5fe1b2f041e36368c5bbe19ca127
Markus Wagner <markus.wagner02@web.de>
2024-12-19 11:19:05 +0100

    add day19

---
+++ Day19a/Worker.cs
@@ -0,0 +1,44 @@
+﻿namespace AdventOfCode2024.Day19a;
+
+public class Worker : IWorker
+{
+    List<string> towels = [];
+    List<string> patterns = [];
+    List<string> impossiblePatterns = [];
+
+    public long DoWork(string inputFile)
+    {
+        var lines = File.ReadAllLines(inputFile);
+        towels = lines[0].Split(", ").ToList();
+        patterns.AddRange(lines.Skip(2));
+
+        var possibles = 0;
+        foreach (var pattern in patterns)
+        {
+            if (IsPossible(pattern))
+            {
+                possibles++;
+            }
+        }
+
+        return possibles;
+    }
+
+    bool IsPossible(string pattern)
+    {
+        if (impossiblePatterns.Contains(pattern))
+        {
+            return false;
+        }
+
+        foreach (var towel in towels.Where(t => pattern.StartsWith(t)))
+        {
+            if (pattern.Length == towel.Length || IsPossible(pattern[towel.Length..]))
+            {
+                return true;
+            }
+        }
+        impossiblePatterns.Add(pattern);
+        return false;
+    }
+}
---
+++ Day19b/Worker.cs
@@ -0,0 +1,46 @@
+﻿namespace AdventOfCode2024.Day19b;
+
+public class Worker : IWorker
+{
+    List<string> towels = [];
+    List<string> patterns = [];
+    Dictionary<string, long> patternCache = [];
+
+    public long DoWork(string inputFile)
+    {
+        var lines = File.ReadAllLines(inputFile);
+        towels = lines[0].Split(", ").ToList();
+        patterns.AddRange(lines.Skip(2));
+
+        var possibles = 0L;
+        foreach (var pattern in patterns)
+        {
+            possibles += IsPossible(pattern);
+        }
+
+        return possibles;
+    }
+
+    long IsPossible(string pattern)
+    {
+        if (patternCache.TryGetValue(pattern, out var isPossible))
+        {
+            return isPossible;
+        }
+
+        var result = 0L;
+        foreach (var towel in towels.Where(t => pattern.StartsWith(t)))
+        {
+            if (pattern.Length == towel.Length)
+            {
+                result++;
+            }
+            else
+            {
+                result += IsPossible(pattern[towel.Length..]);
+            }
+        }
+        patternCache[pattern] = result;
+        return result;
+    }
+}
